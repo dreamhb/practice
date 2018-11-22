95f6103cb64da4ce452acd38942fcfddd9426a1f
Damon Hu <bing.hu1989@gmail.com>
2018-11-22 23:05:39 +0800

    add heap sort implement

--- sort/heap_sort/HeapSort.java
+++ sort/heap_sort/HeapSort.java
@@ -1,10 +1,31 @@
 public class HeapSort {
+
     void buildMaxHeap(int[] arr) {
-    
+        for (int i = arr.length - 1; i >= 0; i--) {
+            maxHeapify(arr, i);
+        }
     }
 
     void maxHeapify(int[] arr, int index) {
-    
+        int left = left(index);
+        int right = right(index);
+        int largest = -1;
+
+        if (left <= arr.heapSize && arr[left] > arr[index]) {
+            largest = left;
+        } else {
+            largest = index;
+        }
+
+        if (right <= arr.heapSize && arr[right] > arr[index]) {
+            largest = right;
+        }
+
+        if (largest != index) {
+            swap(arr[largest], arr[index]);
+            maxHeapify(arr, index);
+        }
+
     }
 
 
