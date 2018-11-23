b9094455cea49badf5b013429aff665fd8f83f75
bing.hu <bing.hu@ecovacs.com>
2018-11-23 16:49:25 +0800

    add heap size

--- sort/heap_sort/HeapSort.java
+++ sort/heap_sort/HeapSort.java
@@ -1,5 +1,7 @@
 public class HeapSort {
 
+    int heapSize = 0;
+
     void buildMaxHeap(int[] arr) {
         for (int i = arr.length - 1; i >= 0; i--) {
             maxHeapify(arr, i);
@@ -7,25 +9,33 @@ public class HeapSort {
     }
 
     void maxHeapify(int[] arr, int index) {
-        int left = left(index);
-        int right = right(index);
-        int largest = -1;
+        int left = index << 1;
+        int right = index << 1 + 1;
+        int largest;
 
-        if (left <= arr.heapSize && arr[left] > arr[index]) {
+        if (left <= heapSize && arr[left] > arr[index]) {
             largest = left;
         } else {
             largest = index;
         }
 
-        if (right <= arr.heapSize && arr[right] > arr[index]) {
+        if (right <= heapSize && arr[right] > arr[index]) {
             largest = right;
         }
 
         if (largest != index) {
-            swap(arr[largest], arr[index]);
+            heapSize++;
+            swap(arr, largest, index);
             maxHeapify(arr, index);
         }
 
+    }
+
+
+    void swap(int[] arr, int l, int r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
     }
 
 
@@ -33,7 +43,7 @@ public class HeapSort {
         buildMaxHeap(arr);
         int heapSize = arr.length;
         for(int i = arr.length - 1; i > 0; i--) {
-            swap(arr[0], arr[i]);
+            swap(arr, 0, i);
             heapSize = heapSize - 1;
             maxHeapify(arr, 0);
         }
