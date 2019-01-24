f20aaeae0b7927d961d1a0f23c48b0c436a40528
bing.hu <bing.hu@ecovacs.com>
2019-01-24 14:27:14 +0800

    add readme

--- dp/fibonacci/Fibonacci.java
+++ dp/fibonacci/Fibonacci.java
@@ -4,10 +4,16 @@ public class Fibonacci {
     public static void main(String[] args) {
         int input = Integer.valueOf(args[0]);
         System.out.println("the dynamic programming fibonacci of the number: " + input + " is " + fibonacciDP(input));
-        //System.out.println("the fibonacci of the number: " + input + " is " + fibonacci(input));
+        System.out.println("the fibonacci of the number: " + input + " is " + fibonacciDPV2(input));
     }
 
 
+    /**
+     * brute force
+     *
+     * time complexity 2^n
+     *
+     * */
     private static long fibonacci(long input) {
 
         if (input == 0) {
@@ -25,8 +31,8 @@ public class Fibonacci {
     static Map<Long, Long> cache = new HashMap<>();
 
     /**
-     * using top-down
-     *
+     * dp top-down
+     * time complexity n^2
      * */
     private static long fibonacciDP(long input) {
         if (cache.containsKey(input)) {
@@ -50,9 +56,37 @@ public class Fibonacci {
 
 
     /**
-     * using bottom-up
-     * TODO
+     * dp bottom-up
+     * time complexity n^2
      */
     private static long fibonacciDPV2(long input) {
+
+        long pre = 0;
+        long prepre = 0;
+        long result = 0;
+        for (int i = 0; i <= input; i++) {
+            if (i == 0) {
+                pre = 0;
+                prepre = 0;
+            }
+
+            if (i == 1) {
+                pre = 0;
+                prepre = 0;
+                result = 1;
+                continue;
+            }
+
+            if (i == 2) {
+                pre = 1;
+                prepre = 0;
+            }
+
+            result = pre + prepre;
+            prepre = pre;
+            pre = result;
+        }
+
+        return result;
     }
 }
---
+++ dp/fibonacci/README.md
@@ -0,0 +1,16 @@
+### Dynamic Programming in Fibonacci
+
+here we use three methods to solve Fibonacci 
+
+##### brute force 
+time complexity is 2^n
+
+#### top-down dp 
+time complexity is n^2
+
+#### bottom-up dp 
+time complexity is n^2
+
+
+### How to anylize the time complexity
+recursive tree
